eb2cddf4a83308a1a9ad678c7f17bf040a20a0c8
hszeroth <859991488@qq.com>
2024-09-03 18:04:01 +0800

    change

---
+++ CommandQueue.cpp
@@ -0,0 +1,22 @@
+#include "CommandQueue.h"
+#include <cassert>
+#include "Helpers.h"
+
+CommandQueue::CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type)
+{
+	m_CommandListType = type;
+	m_d3d12Device = device;
+
+	D3D12_COMMAND_QUEUE_DESC desc = {};
+	desc.Type = m_CommandListType;
+	desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	desc.NodeMask = 0;
+
+	ThrowIfFailed(m_d3d12Device->CreateCommandQueue(&desc, IID_PPV_ARGS(&m_d3d12CommandQueue)));
+
+	m_FenceValue = 0;
+	ThrowIfFailed(m_d3d12Device->CreateFence(m_FenceValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_d3d12Fence)));
+	m_FenceEvent = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	assert(m_FenceEvent && "Failed to create fence event.");
+}
